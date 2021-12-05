19bdef88ab3cdb16f036e5894e005e3bbbb5af32
cavaliba <contact@cavaliba.com>
2021-12-05 19:51:45 +0100

    module load takes cpu_count into account

--- cmt/modules/load.py
+++ cmt/modules/load.py
@@ -3,20 +3,29 @@ import os
 import globals as cmt
 import checkitem
 
+cpu_count = 1
+
 
 def is_above_threshold(load, threshold):
-    if float(load) > float(threshold):
-        return True
-    return False
 
+    if float(load) > float ( cpu_count * threshold ):
+        return True
+
+    return False
 
 def check(c):
 
+    global cpu_count
 
-    threshold1 = c.conf.get('threshold1',999999)
-    threshold5 = c.conf.get('threshold5',999999)
-    threshold15 = c.conf.get('threshold15',999999)
+    threshold1 = c.conf.get('threshold1',5)
+    threshold5 = c.conf.get('threshold5',2)
+    threshold15 = c.conf.get('threshold15',1.5)
 
+
+    cpu_count = os.cpu_count()
+    l = checkitem.CheckItem('cpu_count',cpu_count)
+    l.description='Available CPUs'
+    c.add_item(l)
 
     # (1.17, 0.86, 0.52)
     load = os.getloadavg()
@@ -35,19 +44,21 @@ def check(c):
     c.add_item(l15)
 
     # alerts ?
-    if is_above_threshold(load[0], threshold1):
-        c.severity = cmt.SEVERITY_CRITICAL
-        c.add_message("load1 above threshold : {} > {} ".format(load[0], threshold1))
-        return c
-    if is_above_threshold(load[1], threshold5):
-        c.severity = cmt.SEVERITY_CRITICAL
-        c.add_message("load5 above threshold : {} > {} ".format(load[1], threshold5))
-        return c
     if is_above_threshold(load[2], threshold15):
         c.severity = cmt.SEVERITY_CRITICAL
-        c.add_message("load15 above threshold : {} > {} ".format(load[2], threshold15))
+        c.add_message("load-15/cpu is above threshold : {} > {} ({} cpus)".format(load[2], threshold15 * cpu_count, cpu_count))
+        return c
+
+    if is_above_threshold(load[1], threshold5):
+        c.severity = cmt.SEVERITY_CRITICAL
+        c.add_message("load-5/cpu is above threshold : {} > {} ({} cpus)".format(load[1], threshold5 * cpu_count, cpu_count))
+        return c
+
+    if is_above_threshold(load[0], threshold1):
+        c.severity = cmt.SEVERITY_CRITICAL
+        c.add_message("load-1/cpu is above threshold : {} > {} ({} cpus)".format(load[0], threshold1 * cpu_count, cpu_count))
         return c
 
     # all OK
-    c.add_message("load 1/5/15 min : {}  {}  {}".format(load[0], load[1], load[2]))
+    c.add_message("load 1/5/15 min : {}  {}  {} ({} cpus)".format(load[0], load[1], load[2], cpu_count))
     return c
